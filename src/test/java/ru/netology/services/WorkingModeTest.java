691840ef88b82151cb47d666108e6a2ecc26a9b7
Darey Blitz <dareyblitz@vk.com>
2023-05-17 23:10:48 +0300

    Add ParameterizedTest

--- src/test/java/ru/netology/services/WorkingModeTest.java
+++ src/test/java/ru/netology/services/WorkingModeTest.java
@@ -2,27 +2,33 @@ package ru.netology.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class WorkingModeTest {
-    @Test
-    public void testCountRestMonthsCase1() {
+    @ParameterizedTest
+    @CsvSource({
+            "3,10000,3000,20000",
+            "2,100000,60000,150000"
+    })
+    public void testCountRestMonthsCase(int expected, int income, int expenses, int threshold ) {
         WorkingMode freelancer = new WorkingMode();
 
-        int actual = freelancer.calculate(10_000, 3000, 20_000);
-        int expected = 3;
+        int actual = freelancer.calculate(income, expenses, threshold);
+       // int expected = 3;
 
         Assertions.assertEquals(expected, actual);
     }
 
-    @Test
-    public void testCountRestMonthsCase2() {
-        WorkingMode freelancer = new WorkingMode();
+  //  @Test
+  //  public void testCountRestMonthsCase2() {
+ //       WorkingMode freelancer = new WorkingMode();
 
-        int actual = freelancer.calculate(100_000, 60_000, 150_000);
-        int expected = 2;
+  //      int actual = freelancer.calculate(100_000, 60_000, 150_000);
+ //       int expected = 2;
 
-        Assertions.assertEquals(expected, actual);
-    }
+  //      Assertions.assertEquals(expected, actual);
+ //   }
 }
