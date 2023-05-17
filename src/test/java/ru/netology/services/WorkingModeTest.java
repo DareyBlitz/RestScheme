3816bf72eb80c65e392828368f4e358714609326
Darey Blitz <dareyblitz@vk.com>
2023-05-17 23:25:44 +0300

    Add freelancer.csv

--- src/test/java/ru/netology/services/WorkingModeTest.java
+++ src/test/java/ru/netology/services/WorkingModeTest.java
@@ -3,16 +3,18 @@ package ru.netology.services;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class WorkingModeTest {
     @ParameterizedTest
-    @CsvSource({
-            "3,10000,3000,20000",
-            "2,100000,60000,150000"
-    })
+   // @CsvSource({
+    //        "3,10000,3000,20000",
+    //        "2,100000,60000,150000"
+   // })
+    @CsvFileSource(files="src/test/resources/freelancer.csv")
     public void testCountRestMonthsCase(int expected, int income, int expenses, int threshold ) {
         WorkingMode freelancer = new WorkingMode();
 
